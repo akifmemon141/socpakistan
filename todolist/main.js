6974d9e045af41f091be7ff011269e248ea6e135
Akif Memon <akifmemon141@gmail.com>
2016-07-12 19:59:33 +0500

    todo app added

---
+++ todolist/main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+	
+    $("#click").click(function(){
+		var text = $('#myvalue').val()
+		 $("#container").append('<div id="' + text + '"> <input type="checkbox" name="check" ><label>'+text+' </label><br></div> ');
+		  $("#myvalue").val('');	  
+		  $("#"+text).click(function(){
+			  $("#"+text).remove()
+			  })
+		
+	
+	})
+    
+	
+	
+    
